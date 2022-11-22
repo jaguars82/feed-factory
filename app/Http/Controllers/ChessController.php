07d3d2b0ce967820429e83bbbdb38740d0a3fa15
Egor Sechin <jaguars82@yandex.ru>
2022-11-22 10:57:15 +0300

    render visual chess on page

--- app/Http/Controllers/ChessController.php
+++ app/Http/Controllers/ChessController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Storage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Inertia\Inertia;
 
 class ChessController extends Controller
@@ -15,7 +17,46 @@ class ChessController extends Controller
      */
     public function add(Request $request)
     {
-        return Inertia::render('Chess/Add');
+        $spreadsheet = IOFactory::load(storage_path('app/public/example1.xlsx'));
+        $worksheet = $spreadsheet->getActiveSheet();
+        $highestRow = $worksheet->getHighestRow();
+        $highestColumn = $worksheet->getHighestColumn();
+        $sheetData = array();
+
+        foreach ($worksheet->getRowIterator() as $row) {
+            $cellIterator = $row->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(FALSE);
+            foreach ($cellIterator as $cell) {
+                $cellColumn = $cell->getColumn();
+                $cellRow = $cell->getRow();
+                $cellRawValue = $cell->getValue();
+                if ($cellRawValue === '#NULL!') {
+                    $cellRawValue = null;
+                }
+                $cellItem = [
+                    'address' => $cellColumn.$cellRow,
+                    'row' => $cellRow,
+                    'column' => $cellColumn,
+                    'rawValue' => $cellRawValue,
+                    'bgColor1' => $cell->getStyle()->getFill()->getStartColor()->getRGB(),
+                    'bgColor2' => $cell->getStyle()->getFill()->getEndColor()->getRGB(),
+                    'borders' => [
+                        'top' => $cell->getStyle()->getBorders()->getTop()->getBorderStyle(),
+                        'right' => $cell->getStyle()->getBorders()->getRight()->getBorderStyle(),
+                        'bottom' => $cell->getStyle()->getBorders()->getBottom()->getBorderStyle(),
+                        'left' => $cell->getStyle()->getBorders()->getleft()->getBorderStyle()
+                    ]
+                ];
+                $sheetData[$cellRow][$cellColumn] = $cellItem;
+                //echo '<pre>'; var_dump($cellItem); echo '</pre>'; die;
+            }
+        }
+        //$sheetData = $worksheet->toArray(null, true, true, true);
+        //echo '<pre>'; var_dump($sheetData); echo '</pre>'; die;
+
+        return Inertia::render('Chess/Add', [
+            'chessData' => $sheetData
+        ]);
     }
 
 }
--- resources/js/Pages/Chess/Add.vue
+++ resources/js/Pages/Chess/Add.vue
@@ -2,6 +2,11 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
 
+defineProps({
+  chessData: {
+    type: Array
+  }
+})
 </script>
 
 <template>
@@ -16,12 +21,35 @@ import { Head } from '@inertiajs/inertia-vue3';
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="bg-white overflow-auto shadow-sm sm:rounded-lg">
           <div class="p-6 text-gray-900">
-            форма
+            <div v-for="row of chessData" :key="row" class="flex">
+              <div
+                class="table-cell p-1 border border-gray-200 whitespace-nowrap overflow-visible"
+                :class="[
+                  `bg-[#${cell.bgColor1}]`,
+                  cell.borders.right !== 'none' ? 'border-r-black' : '',
+                  cell.borders.left !== 'none' ? 'border-l-black' : '',
+                  cell.borders.top !== 'none' ? 'border-t-black' : '',
+                  cell.borders.bottom !== 'none' ? 'border-b-black' : '',
+                ]"
+                v-for="cell of row"
+                :key="cell.address"
+              >
+                {{ cell.rawValue }}
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </AuthenticatedLayout>
 </template>
+
+<style scoped>
+.table-cell {
+  width: 70px;
+  min-width: 70px;
+  font-size: 11px;
+}
+</style>
